a8554c16e3b1421ee85942979769b44be2fddf07
Meetkumar Patel <meetkumar.patel@aircanada.ca>
2024-01-09 13:44:07 -0500

    connect mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose ')
 
 // routes
 app.get('/', (req, res)=>{
@@ -8,5 +9,11 @@ app.get('/', (req, res)=>{
 // new 
 app.listen(3000, ()=> {
     console.log('Node API runing on port 3000')
-    
+})
+
+mongoose.connect('mongodb+srv://admin:root@cluster01-2024.n83hj9q.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((e)=>{
+    console.log(e)
 })
