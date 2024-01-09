8d2e9707d5570d9a62d44d5614a4a6a906d5f6b7
Meetkumar Patel <meetkumar.patel@aircanada.ca>
2024-01-09 14:28:58 -0500

    create route to save a product

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose ')
 
+const Product = require('./models/productModel')
+
+app.use(express.json())
+
 // routes
 app.get('/', (req, res)=>{
     res.send('Hello from GET NODE API')
@@ -9,6 +13,17 @@ app.get('/', (req, res)=>{
 // new 
 app.listen(3000, ()=> {
     console.log('Node API runing on port 3000')
+})
+
+app.post('/product', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
 })
 
 mongoose.connect('mongodb+srv://admin:root@cluster01-2024.n83hj9q.mongodb.net/Node-API?retryWrites=true&w=majority')
