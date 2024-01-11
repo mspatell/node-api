756a1ea9e63cd308565600fd3f95824f6b7aaf18
meetkumar.patel <meetkumar.patel@aircanada.ca>
2024-01-10 23:17:22 -0500

    set prettier

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const Product = require('../models/productModel');
-const { route } = require('express/lib/application');
+const express = require("express");
+// const Product = require("../models/productModel");
 const {
-    getProducts, getProduct, addProduct, updateProduct, deleteProduct, 
-} = require('../controllers/productController');
+  getProducts,
+  getProduct,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
 // get all products
-router.get('/', getProducts)
+router.get("/", getProducts);
 
 // get a product by id
-router.get('/:id', getProduct)
+router.get("/:id", getProduct);
 
 // create/add product
-router.post('/', addProduct)
+router.post("/", addProduct);
 
 // update a product
-router.put('/:id', updateProduct)
+router.put("/:id", updateProduct);
 
 // delete a product
-router.delete('/:id', deleteProduct)
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
