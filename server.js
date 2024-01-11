0675a7516781e151c7881757f188406bb403565e
meetkumar.patel <meetkumar.patel@aircanada.ca>
2024-01-10 23:43:01 -0500

    implement errorMiddleware

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const productRoute = require('./routes/productRoute')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/productRoute");
+const errorMiddleware = require("./middleware/errorMiddleware");
 
-const PORT = process.env.PORT || 3000
-const MONGO_URL = process.env.MONGO_URL
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // routes
-app.get('/', (req, res)=>{
-    res.send('Hello from GET NODE API')
-})
+app.get("/", (req, res) => {
+  throw new Error("some error");
+//   res.send('Hello from GET NODE API')
+});
 
-app.use('/api/products', productRoute)
+app.use("/api/products", productRoute);
+app.use(errorMiddleware);
 
-mongoose.connect(MONGO_URL)
-.then(()=>{
-    console.log("Connected to MongoDB")
-    app.listen(PORT, ()=> {
-        console.log(`Node API running on ${PORT}`);
-    })
-}).catch((e)=>{
-    console.log(e)
-})
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Node API running on ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
